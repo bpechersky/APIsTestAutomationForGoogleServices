e282a1fd372f223c56a4bc1c151b7b5b14a26d3a
bpechersky <bpechersky@gmx.com>
2025-06-11 17:12:45 -0700

    place everything in src/test/resources into the test-classes folder automatically.

--- src/test/java/com/expandtesting/api/utils/GoogleOAuthTokenProvider.java
+++ src/test/java/com/expandtesting/api/utils/GoogleOAuthTokenProvider.java
@@ -2,21 +2,31 @@ package com.expandtesting.api.utils;
 
 import com.google.auth.oauth2.GoogleCredentials;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 public class GoogleOAuthTokenProvider {
 
     public static String getAccessToken() throws IOException {
-        GoogleCredentials credentials = GoogleCredentials
-                .fromStream(new FileInputStream("src/test/resources/service-account.json"))
-                .createScoped(List.of("https://www.googleapis.com/auth/userinfo.email"))
-                .createScoped(List.of("https://www.googleapis.com/auth/drive"))
-                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+        InputStream stream = GoogleOAuthTokenProvider.class
+                .getClassLoader()
+                .getResourceAsStream("service-account.json");
 
+        if (stream == null) {
+            throw new FileNotFoundException("service-account.json not found in classpath.");
+        }
+
+        GoogleCredentials credentials = GoogleCredentials
+                .fromStream(stream)
+                .createScoped(List.of(
+                        "https://www.googleapis.com/auth/userinfo.email",
+                        "https://www.googleapis.com/auth/drive",
+                        "https://www.googleapis.com/auth/cloud-platform"
+                ));
 
         credentials.refreshIfExpired();
-        return credentials.getAccessToken().getTokenValue();  // ✅ Return the token here
+        return credentials.getAccessToken().getTokenValue();
     }
 }
